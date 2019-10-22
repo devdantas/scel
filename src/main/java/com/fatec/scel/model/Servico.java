c34070222bc75164d0283f07ab9167f5824de198
MAURICIO CORREIA DANTAS <mauricio.dantas@wds.local>
2019-10-22 10:29:01 -0300

    Emprestimo ok

--- src/main/java/com/fatec/scel/model/Servico.java
+++ src/main/java/com/fatec/scel/model/Servico.java
@@ -1,30 +1,12 @@
 package com.fatec.scel.model;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 import org.joda.time.*;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
+import org.springframework.stereotype.Service;
 
+@Service
 public class Servico {
-	/**
-	 * valida o formato da data
-	 *
-	 * @param data no formato yyyy/MM/dd
-	 * @return true se a data estiver no formato valido e false para formato
-	 *         invalido
-	 */
-	public boolean validaData(String data) {
-		DateFormat df = new SimpleDateFormat("yyyy/MM/dd");
-		df.setLenient(false); //
-		try {
-			df.parse(data); // data válida
-			return true;
-		} catch (ParseException ex) {
-			return false;
-		}
-	}
 
 	/**
 	 * Objetivo - verificar se houve atraso na devolucao
@@ -33,6 +15,7 @@ public class Servico {
 	 * @return int < 0 se estiver atrasado e > 0 se estiver no prazo retorna nulo se
 	 *         o objeto emprestimo é nulo.
 	 */
+
 	public Integer verificaAtraso(Emprestimo umEmprestimo) {
 		if (umEmprestimo != null) {
 			DateTimeFormatter fmt = DateTimeFormat.forPattern("YYYY/MM/dd");
@@ -44,4 +27,5 @@ public class Servico {
 			return null;
 		}
 	}
+
 }
